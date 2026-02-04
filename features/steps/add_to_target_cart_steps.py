03a2bf0dbeeaf9302b4640afced42c07cb164c54
Rollyn Leigh <jordanrollyn07@gmail.com>
2026-02-03 19:44:56 -0600

    Homework 5 - Add to cart w/ wait -loops

--- features/steps/add_to_target_cart_steps.py
+++ features/steps/add_to_target_cart_steps.py
@@ -9,13 +9,14 @@ from selenium. webdriver.support import expected_conditions as EC
 def click_add_to_cart(context):
     sleep(20)
     context.driver.find_element(By.CSS_SELECTOR, "[data-test='chooseOptionsButton']").click()
-    context.driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test="shippingButton"]'))).click()
+    sleep(5)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Fulfillment"] [id*="addToCartButtonOrTextIdFor"]'))).click()
 
 
 @then('Confirm added to cart message from side navigation')
 def added_to_cart_message_from_side_navigation(context):
-
-    context.driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h2[data-test="modal-drawer-heading"] span[class="h-text-lg"]')))
+    sleep(5)
+    actual_text = context.driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h2[data-test="modal-drawer-heading"] span[class="h-text-lg"]'))).text
 
     expected_text = 'Added to cart'
-    assert f'Expected text to be {expected_text}'
+    assert actual_text==expected_text, f'Expected text to be {expected_text}, but got {actual_text}'
--- features/tests/add_to_target_cart.feature
+++ features/tests/add_to_target_cart.feature
@@ -4,7 +4,7 @@ Feature: User can add any product to cart
 
     Scenario: User can add product to cart
         Given Open target main page
-        When Search for coffee
+        When Search for tea
         And Click on add to cart button
         Then Confirm added to cart message from side navigation
 
